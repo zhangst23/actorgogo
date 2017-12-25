72e730747c957f97dfa774e951ad120e25f0e93f
栋栋晓 <2243980491@qq.com>
2017-12-25 20:31:54 +0800

    img show

--- actorgogo/settings.py
+++ actorgogo/settings.py
@@ -126,8 +126,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace("\\", "/")
+MEDIA_URL = '/upload/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'upload').replace("\\", "/")
 
 
 
